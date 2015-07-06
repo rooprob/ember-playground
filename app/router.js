fab85e822779c098f89dde7c31d07cdaf2012b19
Robert Fielding <fieldingrp@gmail.com>
2015-07-06 20:08:36 +1200

    Add demo components from http://www.programwitherik.com/ember-js-components-examples/

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.resource('solutions', { path: '/' });
+  this.resource('about-profile', { path: '/about' });
 });
 
 export default Router;
