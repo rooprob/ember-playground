f92551da1459e3b6dccf221806ec3c92f772341e
Robert Fielding <fieldingrp@gmail.com>
2015-07-12 19:51:10 +1200

    WIP navbar menu activation

--- app/components/main-nav/component.js
+++ app/components/main-nav/component.js
@@ -4,14 +4,15 @@ var computed = Ember.computed;
 var service = Ember.inject.service;
 
 export default Ember.Component.extend({
-
+  tagName: 'nav',
+  classNames: ['nav', 'navbar-inverse', 'navbar-fixed-top' ],
   store: service(),
 
   mainNav: computed(function() {
     var states = new Ember.Set();
     states.add(Ember.Object.create({
       location: 'solutions-route',
-      active: null,
+      active: 'active',
       title: 'Main',
     }));
     states.add(Ember.Object.create({
